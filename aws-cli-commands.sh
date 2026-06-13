7449d31db8da5d044db9db275accc0a59d5fe04d
kiritoaftab <aftabemir123@gmail.com>
2026-06-13 19:14:40 +0530

    Basic AWS setup and IAM creation with Admin User, of default AWS user group

---
+++ aws-cli-commands.sh
@@ -0,0 +1,8 @@
+# Command to check the AWS CLI version
+aws --version
+# Command to configure AWS CLI with access key, secret key, and default region
+aws configure
+# Command to list the current AWS Identity
+aws sts get-caller-identity
+# Command to list all S3 buckets in the account
+aws s3 ls
